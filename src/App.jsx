a7c6afea434ce8e1200ee1b31835e5cdde029099
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-03-25 13:17:04 +0000

    propsCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Card from './components/Card'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import KatiePic from '../src/images/katie-zaferes.png'
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <Hero /> */}
-      <Card />
+      <Card
+        pic={KatiePic}
+        rating={5}
+        review={6}
+        country="USA"
+        description="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,11 @@
-import KatiePic from '../images/katie-zaferes.png'
 import Star from '../images/star.png'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="pl-12 pt-10 text-lg">
       <div className="relative">
         <img
-          src={KatiePic}
+          src={props.pic}
           alt="Kaitie zaferes swimming"
           className="rounded-2xl"
         />
@@ -20,13 +19,15 @@ export default function Card() {
       <div className="leading-10">
         <div className="flex items-center gap-2 pt-2 ">
           <img src={Star} alt="Star" />
-          <p>5.0</p>
-          <h4 className="font-thin text-gray-400 ">(6) • USA </h4>
+          <p>{props.rating}</p>
+          <h4 className="font-thin text-gray-400 ">
+            ({props.review}) • {props.country}{' '}
+          </h4>
         </div>
         <div>
-          <h4>Life lessons with Katie Zaferes</h4>
+          <h4>{props.description}</h4>
           <p>
-            <span className="font-bold">From $136</span> / person
+            <span className="font-bold">From ${props.price}</span> / person
           </p>
         </div>
       </div>
